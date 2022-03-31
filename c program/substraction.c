833e0d34199f93fd6b3c47be86c738c911bd7c2f
upadhyaypriyanka <upadhyaypriyanka509@gmail.com>
2022-03-31 21:28:09 +0530

    sustraction

---
+++ c program/substraction.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+
+int num1,num2,sub;
+printf("enter the value of num1 and num2 \n");
+scanf("%d %d",&num1,&num2);
+sub=num1-num2;
+printf("%d",sub);
+}
