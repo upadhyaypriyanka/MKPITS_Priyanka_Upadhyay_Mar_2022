c2dd3a50f1e32adb84ee36fd19b991b00beceb0f
upadhyaypriyanka <upadhyaypriyanka509@gmail.com>
2022-03-31 22:57:24 +0530

    multiplication

---
+++ c program/multiplication.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c;
+	printf("Enter the value of a and b");
+	scanf("%d %d",&a,&b);
+	c=a*b;
+	printf("multiplication of a and b is %d",c);
+}
