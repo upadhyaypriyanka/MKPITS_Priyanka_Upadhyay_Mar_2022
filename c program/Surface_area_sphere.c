b18e89e732c7ffa5dca29ff8c8a78cf1edde74c5
upadhyaypriyanka <upadhyaypriyanka509@gmail.com>
2022-04-02 13:06:03 +0530

    Assignment-1

---
+++ c program/Surface_area_sphere.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int r;
+	printf("Enter the value of r\n");
+	scanf("%d",&r);
+    float pi=3.14, surface;
+    surface=4*pi*r*r;
+    printf(" Total Surface Area Of Sphere :%f\n",surface);
+    
+}
