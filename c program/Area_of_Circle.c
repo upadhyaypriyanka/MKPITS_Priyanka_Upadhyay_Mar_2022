32420d4db70783a19f892b5ac22c8771f60212d5
upadhyaypriyanka <upadhyaypriyanka509@gmail.com>
2022-04-02 10:20:46 +0530

    Assignment-1

---
+++ c program/Area_of_Circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int r;
+	printf("Enter the value of radius\n");
+	scanf("%d",&r);
+	float area=3.14*r*r;
+	printf("Area of circle : %f\n",area);
+	
+}
