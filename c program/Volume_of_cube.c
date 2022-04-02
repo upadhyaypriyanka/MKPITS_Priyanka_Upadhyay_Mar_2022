e0726c9fcac318956a1ad00a22abbe2193a217a4
upadhyaypriyanka <upadhyaypriyanka509@gmail.com>
2022-04-02 14:16:05 +0530

    Assignment-1

---
+++ c program/Volume_of_cube.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,volume;
+	printf("Enter the value of edge\n");
+	scanf("%d",&a);
+	volume=a*a*a;
+	printf("Volume of cube is: %d\n",volume);
+}
