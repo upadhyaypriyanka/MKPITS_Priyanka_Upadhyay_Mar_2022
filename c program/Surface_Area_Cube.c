cecc589027d4430aec6e3e1133d9811bc6163018
upadhyaypriyanka <upadhyaypriyanka509@gmail.com>
2022-04-02 10:55:53 +0530

    Assignment-1

---
+++ c program/Surface_Area_Cube.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a;
+	printf("Enter the value of a\n");
+	scanf("%d",&a);
+	int s=6*(a*a); // 6a2
+	printf("Surface Area Of Cube : %d\n",s);
+}
