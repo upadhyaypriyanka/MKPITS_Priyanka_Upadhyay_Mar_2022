12e8b8224d11908ae70f0e95ada2576a7a293d84
upadhyaypriyanka <upadhyaypriyanka509@gmail.com>
2022-04-02 10:37:13 +0530

    Assignment-1

---
+++ c program/Area_of_ellipse.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b;
+	printf("Enter the value of axis a\n");
+	scanf("%d",&a);
+	printf("Enter the value of axis b\n");
+	scanf("%d",&b);
+  float	Area=3.14*a*b;
+	printf("Area of ellipse is: %f\n",Area);
+}
