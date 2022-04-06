9c1f0eb176949d75d61fe78efe0186ea5ef586bc
upadhyaypriyanka <upadhyaypriyanka509@gmail.com>
2022-04-06 22:54:10 +0530

    Assignment-2

---
+++ c program/perimeter_circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int radius=6;
+	float pi=3.14;
+	float peri=2*pi*radius;
+	printf("perimeter of circle is : %f\n",peri);
+	float area=pi*radius*radius;
+	printf("area of circle is : %f\n",area);
+	
+}
