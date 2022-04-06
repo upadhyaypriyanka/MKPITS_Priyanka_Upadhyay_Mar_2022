629479b4c052086a5701b8731309b7c548e0fb59
upadhyaypriyanka <upadhyaypriyanka509@gmail.com>
2022-04-06 22:51:51 +0530

    Assignment-2

---
+++ c program/perimeter_rect.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int height=7,width=5;
+	int peri= 2*(height+width);
+	printf("perimeter of a rectangle is :%d\n",peri);
+	int area = height*width;
+	printf("area of a rectangle is : %d\n",area);
+}
