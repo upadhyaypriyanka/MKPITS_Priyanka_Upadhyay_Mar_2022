3aec486123decb470e2a53c1b631370b55f89479
upadhyaypriyanka <upadhyaypriyanka509@gmail.com>
2022-04-06 21:26:21 +0530

    Assignment-2

---
+++ c program/F_C.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	printf("  #######\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#####\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n\n\n\n");
+	
+	
+	printf("  ######\n");
+	printf(" ######\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf(" ######\n");
+	printf("  ######\n");
+
+	
+	
+}
