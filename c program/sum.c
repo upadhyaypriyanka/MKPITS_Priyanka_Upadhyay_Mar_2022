61fcd2ca684dacac5f5388e45fe622502f7ff149
upadhyaypriyanka <upadhyaypriyanka509@gmail.com>
2022-04-06 21:23:44 +0530

    Assignment-2

---
+++ c program/sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,add;
+	printf("Enter the value of a\n");
+	scanf("%d",&a);
+	printf("Enter the value of b\n");
+	scanf("%d",&b);
+	add=a+b;
+	printf("Addition of two number is :%d\n",add);
+}
