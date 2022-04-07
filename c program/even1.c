a45bdd32f66262d97696ac9b5243cf7330693e94
upadhyaypriyanka <upadhyaypriyanka509@gmail.com>
2022-04-07 17:14:27 +0530

    even_odd

---
+++ c program/even1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,r;
+	printf("Enter value\n");
+	scanf("%d",&a);
+	 r=a%2;
+if(r==0)
+{
+	printf("value is even \n",a);
+	
+}
+	 
+	else
+	{
+		printf("value is odd  \n",a);
+	}
+	
+	
+}
