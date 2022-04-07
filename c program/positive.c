75ff6fdcee20f01ff41ddc5ea4dffab1f70a280b
upadhyaypriyanka <upadhyaypriyanka509@gmail.com>
2022-04-07 17:17:37 +0530

    positive_negative

---
+++ c program/positive.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a;
+	printf ("Enter the number\n");
+	scanf("%d",&a);
+	if(a>=0)
+	{
+		printf("number is positive\n");
+	}
+	else
+	{
+printf("number is negative\n");
+}
+}
