526e2a51b2b7b9e0971b28b167e2effe67dc40b5
upadhyaypriyanka <upadhyaypriyanka509@gmail.com>
2022-04-09 18:50:57 +0530

    Assignment-4

---
+++ c program/even_q26.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i;
+printf("even numbers from 1 to 50\n");
+	i=1;
+	while(i<=50) 
+	{
+		if(i%2==0)
+		{
+			printf("%d is even number\n",i);
+			
+		}
+		i++;
+	}
+	
+}
