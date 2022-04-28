c572667e6881b6c5d8d121cf9853eef277bf6ad8
upadhyaypriyanka <upadhyaypriyanka509@gmail.com>
2022-04-28 19:03:01 +0530

    Assignment_string

---
+++ c program/string_in_built_strcat.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+	char name1[25]={" priyanka "};
+	char name2[25]={"upadhyay"};
+	strcat(name1,name2);
+	printf("%s",name1,name2);
+}
