903be8717c063c4aef3b00b614240ef573aeaee6
upadhyaypriyanka <upadhyaypriyanka509@gmail.com>
2022-04-28 18:05:09 +0530

    Assignment_string

---
+++ c program/string_rev.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+	char name[25],n,rev;
+	printf("Enter name\n");
+	scanf("%s",name);
+	 strrev(name);
+	printf("%s",name);
+	
+}
