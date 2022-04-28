a14717a344e34c4f81cae675ef4e44334c74a198
upadhyaypriyanka <upadhyaypriyanka509@gmail.com>
2022-04-28 13:09:21 +0530

    Assignment-6

---
+++ c program/array_triple_q45.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a[6],i,n;
+	printf("enter number\n");
+	scanf("%d",&n);
+	for(i=0;i<=6;i++)
+	{
+		n=n*3;
+	a[i]=n;
+		
+	}
+		for(i=0;i<=6;i++)
+	{
+		
+		
+		printf(" a[%d]=%d\n",i,a[i]);
+		
+	}
+	
+	
+}
