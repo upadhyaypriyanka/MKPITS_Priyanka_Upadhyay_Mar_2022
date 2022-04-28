695dcec30fbc2d1934f0aca1ae81b1a20d26057e
upadhyaypriyanka <upadhyaypriyanka509@gmail.com>
2022-04-28 17:54:15 +0530

    lacture_work

---
+++ c program/function_name.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void disname();
+void main()
+{
+int i;
+for(i=1;i<=10;i++)
+{
+	disname();
+	}	
+	
+
+}
+void disname()
+{
+	printf(" My name is Priyanka\n");
+}
