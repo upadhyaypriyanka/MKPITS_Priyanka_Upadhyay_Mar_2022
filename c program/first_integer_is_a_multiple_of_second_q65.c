8670fa89f850059234322dfd1c14c4d5551517e2
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-05-03 15:46:35 +0530

    Assignment-8

---
+++ c program/first_integer_is_a_multiple_of_second_q65.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b;
+	printf("Enter the value of a\n");
+	scanf("%d",&a);
+	printf("Enter the value of b\n");
+	scanf("%d",&b);
+	if(a%b==0)
+	{
+		printf("%d is multiple of %d\n",a,b);
+	}
+	else
+	{
+		printf("%d in not multiple of %d\n",a,b);
+	
+		
+	}
+	}
