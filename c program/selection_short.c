80c4c72cd618c29c39499fb003359f0277ced6e4
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-05-05 10:31:42 +0530

    lacture_work

---
+++ c program/selection_short.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a[10],i,temp,j;
+	for(i=0;i<=9;i++)
+	{
+		printf("a[%d]=",i);
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<=9;i++)
+	{
+		for(j=i+1;j<=9;j++)
+		{
+			if(a[i]>a[j])
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	printf("After sorting\n");
+	for(i=0;i<=9;i++)
+	{
+		printf("%d ",a[i]);
+	}
+}
