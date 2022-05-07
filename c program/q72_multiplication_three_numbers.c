d20048d0d4a19a95365eab997a2691f139a07154
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-05-07 18:47:17 +0530

    Assignment-9

---
+++ c program/q72_multiplication_three_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c,d;
+	printf("enter first number\n");
+	scanf("%d",&a);
+	printf("enter second number\n");
+	scanf("%d",&b);
+	printf("enter third number\n");
+	scanf("%d",&c);
+	d=a*b*c;
+	printf("multiplication of three numbers is %d",d);
+}
