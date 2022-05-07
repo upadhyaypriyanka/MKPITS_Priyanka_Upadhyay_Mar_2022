6b82c0934427ac175dff2455ab9cff8c70d7883c
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-05-07 18:59:21 +0530

    Assignment-9

---
+++ c program/q71_specified_operations.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	printf("(-1+4*6) is = %d\n",(-1+4*6));
+	printf("(35+5)%7 is = %d\n",(35+5)%7);
+	printf("14+-4*6/11 is = %d\n",14+-4*6/11);
+	printf("2+15/6*1-7%2 is = %d\n",2+15/6*1-7%2);
+}
