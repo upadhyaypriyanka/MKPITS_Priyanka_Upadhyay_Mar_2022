2291c9da696d8212eedf92bfd834bee1764be48d
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-05-07 22:02:26 +0530

    Assignment-9

---
+++ c program/q78_multiple_of_3_or_7.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a;
+	printf("Enter integer\n");
+	scanf("%d",&a);
+	if(a%3==0)
+	{
+		printf("%d is multiple of 3\n",a);
+	}
+	if(a%7==0)
+	{
+	printf("%d is multiple of 7\n",a);	
+	}
+	
+
+}
