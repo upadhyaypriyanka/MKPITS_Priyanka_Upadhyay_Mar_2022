49eedc9e21993bab7b0881aa6c803686b9d78c2b
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-05-08 13:48:47 +0530

    Assignment-9

---
+++ c program/q79_temperature.c
@@ -0,0 +1,18 @@
+#include<conio.h>
+#include<conio.h>
+void main()
+{
+	int temp1,temp2;
+	printf("Enter temp 1\n");
+	scanf("%d",&temp1);
+	printf("Enter temp 2\n");
+	scanf("%d",&temp2);
+	if(temp1<0 && temp2>100)
+	{
+	printf("1");
+	}
+	else{
+		printf("0");
+	}
+	
+}
