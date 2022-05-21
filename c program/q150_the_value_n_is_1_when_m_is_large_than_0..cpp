36448efed30d437c4234d0731f0b45219a22b01a
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-05-21 12:53:22 +0530

    Assignment-16

---
+++ c program/q150_the_value_n_is_1_when_m_is_large_than_0..cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int m,n;
+	printf("Enter the value of m\n");
+	scanf("%d",&m);
+	if(m>0)
+	{
+		printf("n is 1\n");
+		
+	}
+	if(m==0)
+	{
+		printf("n is 0\n");
+	}
+	if(m<0)
+	{
+		printf("n is -1");
+	}
+	
+}
