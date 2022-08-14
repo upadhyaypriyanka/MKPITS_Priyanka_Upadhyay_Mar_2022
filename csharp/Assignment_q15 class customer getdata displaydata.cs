2b786b478adde546a55f5ee819851fdd703bf0b1
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:13:58 +0530

    practice_q21

---
+++ csharp/Assignment_q15 class customer getdata displaydata.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crate_a_class_customer_having_fields
+{
+    class Customer
+    {
+        public int cusno;
+        public string cusname;
+        public string cusadd;
+        public string custelno;
+
+       public void getcustomerdata()
+        {
+            Console.WriteLine("Enter customer no ");
+            cusno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter customer name");
+            cusname = Console.ReadLine();
+            Console.WriteLine("Enter customer address");
+            cusadd = Console.ReadLine();
+            Console.WriteLine("Enter customer phone no.");
+            custelno = Console.ReadLine();
+
+        }
+        public void displaycustomerdata()
+        {
+            Console.WriteLine("Customer no = " + cusno);
+            Console.WriteLine("Customer name = " + cusname);
+            Console.WriteLine("Customer address = " + cusadd);
+            Console.WriteLine("Customer phone no. = " +custelno);
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer cus = new Customer();
+            cus.getcustomerdata();
+            cus.displaycustomerdata();
+          
+            Console.ReadLine();
+        }
+    }
+}
