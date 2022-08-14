63a2f521b8f8e0791341bf9b9bcd407799796c15
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:21:21 +0530

    practice_q33

---
+++ csharp/eligible to vote or not.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eligible_to_vote_or_not
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { // wap to accept a age from the user and print whether eligible to vote or not
+            byte age;
+            Console.WriteLine("Enter age");
+            age = Convert.ToByte(Console.ReadLine());
+            if (age <= 17)
+            {
+                Console.WriteLine("You are not eligible for vote");
+            }
+            else
+            {
+                Console.WriteLine("You are eligible for vote");
+            }
+            Console.ReadLine();
+        }
+    }
+}
