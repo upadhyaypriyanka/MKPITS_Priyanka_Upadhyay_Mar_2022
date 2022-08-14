ccb079403b87cb2428434f4ead0c7afc421240a6
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-14 22:28:29 +0530

    addition

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -4,25 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace accept_character_and_name_from_the_user
+namespace Addition
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // wap to accept character from the user and print it
-
-            char ch;
-            Console.WriteLine("Enter character");
-            ch = Convert.ToChar(Console.ReadLine());
-            Console.WriteLine("character entered: " + ch);
-
-            // wap to accept name from the user and print it
-
-            string name;
-            Console.WriteLine("Enter your name");
-            name = Console.ReadLine();
-            Console.WriteLine("Name entered: " + name);
+            int num1,num2;
+            Console.WriteLine("Enter first number");
+            num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            num2=Convert.ToInt32(Console.ReadLine());
+            int add=num1+num2;
+            Console.WriteLine("Addition of two number is: "+add);
             Console.ReadLine();
         }
     }
