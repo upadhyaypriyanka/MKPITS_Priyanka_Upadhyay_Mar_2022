2f5cfdd3ee366ef6421e34f778b42175c76755eb
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:24:01 +0530

    practice_q36

---
+++ csharp/example of method overloading.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example_of_method_overloading
+{
+    internal class Program
+    {
+        static int add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        static int add(int num1, int num2,int num3)
+        {
+            return num1 + num2+num3;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(add(2, 6));
+            Console.WriteLine(add(2, 6,9));
+            Console.ReadLine();
+        }
+    }
+}
