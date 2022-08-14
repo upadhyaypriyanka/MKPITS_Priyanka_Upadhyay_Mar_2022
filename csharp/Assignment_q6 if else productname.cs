1ddbac3a9f0548c5b072013af22aad15e1d2d5c7
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:07:33 +0530

    practice_q12

---
+++ csharp/Assignment_q6 if else productname.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace productname_productrate_productquantity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+// wap to accept productname,productrate,quantity display total amount
+        {
+            string pname;
+            int prate, qua,total;
+            Console.WriteLine("Enter product name");
+            pname=Console.ReadLine();
+            Console.WriteLine("Product name = " + pname);
+            Console.WriteLine("Enter product rate");
+            prate = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Product rate = " + prate);
+            Console.WriteLine("Enter quantity");
+            qua = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Quantity = " + qua);
+            total = prate * qua;
+            Console.WriteLine("Total amount = " + total);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
