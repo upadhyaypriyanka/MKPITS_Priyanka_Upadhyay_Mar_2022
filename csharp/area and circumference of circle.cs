dc821b8dad92f695a5a22edc68b9d364e3ec273e
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:00:32 +0530

    practice_q4

---
+++ csharp/area and circumference of circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_and_circumference_of_circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int radius;
+            Console.WriteLine("Enter radius");
+            radius = Convert.ToInt32(Console.ReadLine());
+            float area = 3.14f * radius * radius;
+            Console.WriteLine("Area of circle is: " + area);
+            float circ = 2 * 3.14f * radius;
+            Console.WriteLine("Circumference of circle is: " + circ);
+            Console.ReadLine();
+        }
+    }
+}
