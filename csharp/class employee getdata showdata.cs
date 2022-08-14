28aa03471622a2682ac62f16affdbbca0fb26867
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:17:50 +0530

    practice_q27

---
+++ csharp/class employee getdata showdata.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_employee_getdata_showdata
+{
+    internal class Program
+    {   class Employee
+        {
+             int empno;
+              string empname;
+               float salary;
+            //creating methods
+            public void getdata()
+            {
+                Console.WriteLine("Enter employee no");
+                empno = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter employee name");
+                empname = Console.ReadLine();
+                Console.WriteLine("Enter employee salary");
+                salary = Convert.ToSingle(Console.ReadLine());
+            }
+            public void showdata()
+            {
+                Console.WriteLine("empno = " + empno);
+                Console.WriteLine("empname = " + empname);
+                Console.WriteLine("empsalary = " + salary);
+                Console.ReadLine();
+            }
+        }
+       
+        
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.getdata();
+            emp.showdata();
+        }
+    }
+}
