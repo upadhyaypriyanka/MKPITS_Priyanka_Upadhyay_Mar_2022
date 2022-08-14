5f4da9c3975bfdafa97740717db736c5d3ac1f19
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:23:00 +0530

    practice_q34

---
+++ csharp/emp accept value from the user.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_employee_accept_value_from_the_user
+{
+    class Employee
+    {
+        public int empno;
+        public string empname;
+        public float salary;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            Console.WriteLine("Enter empno ");
+            emp.empno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter empname ");
+            emp.empname = Console.ReadLine();
+            Console.WriteLine("Enter empsalary ");
+            emp.salary = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Empno = " + emp.empno);
+            Console.WriteLine("Empname = " + emp.empname);
+            Console.WriteLine("empsalary = " + emp.salary);
+            Console.ReadLine();
+
+        }
+    }
+}
