6102922f688d0e8c85b0312ecab2a1b7a8d7936d
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:15:30 +0530

    practice_q24

---
+++ csharp/Assignment_q18 enter 5 student name in array.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student_name_in_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] std = new string[5];
+            Console.WriteLine("Enter first name");
+            std[0]= Console.ReadLine();
+            Console.WriteLine("Enter second name");
+            std[1] = Console.ReadLine();
+            Console.WriteLine("Enter third name");
+            std[2] = Console.ReadLine();
+            Console.WriteLine("Enter fourth name");
+            std[3] = Console.ReadLine();
+            Console.WriteLine("Enter fifth name");
+            std[4] = Console.ReadLine();
+            for(int i = 0; i < std.Length; i++)
+            {
+                Console.WriteLine(std[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
