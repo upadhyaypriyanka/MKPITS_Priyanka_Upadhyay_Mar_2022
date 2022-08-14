178301e716126781b3699e18f45de62a9c8214d7
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:07:06 +0530

    practice_q11

---
+++ csharp/Assignment_q5 if else empname.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emno_empname_empdesignation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        { // wap to empno,empname,designation calculaye and display bonus i.e 10000 for manager 5000 for clerk 2000 for peon
+            
+            int bonus = 0;
+            string n,name, designation;
+            Console.WriteLine("Enter employee phone number");
+            n = Console.ReadLine();
+            Console.WriteLine("Employee phone number= " + n);
+            Console.WriteLine("Enter employee name");
+            name = Console.ReadLine();
+            Console.WriteLine("Employee Name= " + name);
+            Console.WriteLine("Enter Designation");
+            designation = Console.ReadLine();
+            if (designation == "manager")
+            {
+                bonus = 10000;
+            }
+            else if (designation == "clerk")
+            {
+                bonus = 5000;
+            }
+            else if (designation == "peon")
+            {
+                bonus = 2000;
+            }
+            else
+            {
+                Console.WriteLine("Invalid designation");
+            }
+            Console.WriteLine("your bonus is= " + bonus);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
