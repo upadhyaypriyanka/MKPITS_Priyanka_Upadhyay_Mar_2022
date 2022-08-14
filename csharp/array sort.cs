a921aba3d05b1d55c15ce2fabe1c8fe836d72dcd
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:03:38 +0530

    practice_q8

---
+++ csharp/array sort.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_short
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 6, 9, 3, 2, 0, 9 };
+            // Array.Sort(arr);
+            //Array.Clear(arr,0, 6);
+            int[] arr1 = (int[])arr.Clone();
+          for(int i=0;i<arr1.Length;i++)
+            {
+                Console.Write(arr1[i] + "\t");
+            }
+            Console.ReadLine();
+        }
+    }
+}
