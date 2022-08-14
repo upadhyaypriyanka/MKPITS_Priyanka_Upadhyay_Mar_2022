f2b81514f2b42785cf58ab1a22f8e02b3b7e27b8
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:35:26 +0530

    practice_q55

---
+++ csharp/sum in array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum_in_array
+{
+    internal class Program
+    {   static void display(params int[] num)
+        {
+            int sum = 0;
+            for (int i=0; i < num.Length; i++)
+            {
+                
+                sum = sum + num[i];
+            }
+            Console.WriteLine("sum = {0}",sum);
+        }
+        static void Main(string[] args)
+        {
+            display(5, 7, 8);
+            Console.ReadLine();
+        }
+    }
+}
