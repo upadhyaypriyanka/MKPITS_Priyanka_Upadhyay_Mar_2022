93b2edbb0e27c4685926e95d34ad0acb734f1202
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:20:27 +0530

    practice_q31

---
+++ csharp/display without parameter returning nothing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace display_without_parameter_returning_nothing
+{
+    internal class Program
+    {
+        static void display()
+        {
+            Console.WriteLine("Hello from display method");
+        }
+        static void Main(string[] args)
+        {
+            display();
+            Console.WriteLine("Bye");
+            Console.ReadLine();
+        }
+    }
+}
