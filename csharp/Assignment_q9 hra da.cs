cc7281c96dcdd02a13107045f5479835b83d721f
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:09:49 +0530

    practice_q14

---
+++ csharp/Assignment_q9 hra da.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace find_hra_da
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // wap to accept empno,empname,basicsalary and calculate hra ie 32% of basic, da ie 45% of basic display empno,empname,hra,da,tatal salary
+            string no,name;
+            int salary;
+
+            Console.WriteLine("Enter employee no.");
+            no = Console.ReadLine();
+            Console.WriteLine("Employee no. " + no);
+
+            Console.WriteLine("Enter employee name");
+            name = Console.ReadLine();
+            Console.WriteLine("Employee no. " + name);
+
+            Console.WriteLine("Enter employee basic salary");
+            salary = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Employee basic salary " + salary);
+
+            float hra = salary * 0.32f;
+            Console.WriteLine("hra = " + hra);
+            float da = salary * 0.45f;
+            Console.WriteLine("da = " + da);
+            float total = salary + hra + da;
+            Console.WriteLine(" Total salary = " + total);
+            Console.ReadLine();
+
+        }
+    }
+}
