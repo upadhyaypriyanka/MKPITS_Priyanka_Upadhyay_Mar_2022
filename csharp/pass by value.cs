bd0f08731ae7f9bc5e36c16521bc0ea1e432ea93
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:31:42 +0530

    practice_q49

---
+++ csharp/pass by value.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pass_by_value
+{
+    internal class Program
+    {
+        static void display(int num)
+        {
+            num++;
+            Console.WriteLine("value inside method " + num);
+        }
+        static void Main(string[] args)
+        {
+            int n = 7;
+            display(n);
+            Console.WriteLine("value inside main " + n);
+            Console.ReadLine();
+        }
+    }
+}
