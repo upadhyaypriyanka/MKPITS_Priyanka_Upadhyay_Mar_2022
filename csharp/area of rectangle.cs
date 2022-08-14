56c68d8f70387c4e31adc6e0fed9d82f41707600
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:00:54 +0530

    practice_q5

---
+++ csharp/area of rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q3.area_of_rectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length, breadth;
+            Console.WriteLine("Enter length");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter breadth");
+            breadth = Convert.ToInt32(Console.ReadLine());
+            int rect = length * breadth;
+            Console.WriteLine("Area of rectangle is: " + rect);
+            Console.ReadLine();
+
+        }
+    }
+}
