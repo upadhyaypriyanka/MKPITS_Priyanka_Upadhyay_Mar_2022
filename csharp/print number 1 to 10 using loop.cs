5c495bd3453d418b719f5072463e1c6208ffb335
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:33:07 +0530

    practice_q51

---
+++ csharp/print number 1 to 10 using loop.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_no_1_to_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // wap to print number from 1 to 10 using while loop
+            int i=1;
+            Console.WriteLine("numbers 1 to 10 using while loop");
+            while (i <= 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            
+
+            // wap to print number from 1 to 10 using do while loop
+            int j = 1;
+            Console.WriteLine("numbers 1 to 10 using do while loop");
+            do
+            {
+                Console.WriteLine(j);
+                j++;
+            } while (j <= 10);
+
+            // wap to print number from 1 to 10 using for loop
+            int k;
+            Console.WriteLine("numbers 1 to 10 using for loop");
+            for (k = 1; k <= 10; k++)
+            {
+                Console.WriteLine(k);
+            }
+            Console.ReadLine();
+        }
+    }
+}
