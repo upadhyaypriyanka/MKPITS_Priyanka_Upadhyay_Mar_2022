8955cec17890e394f3375252eaa62f76b54b6e88
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:04:28 +0530

    practice_q9

---
+++ csharp/Assignment_q3 greater number.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greater_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // wap to accept two number and print greater number
+            int num1, num2;
+            Console.WriteLine("Enter first number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2)
+            {
+                Console.WriteLine(" greater number is " + num1);
+            }
+            else
+            {
+                Console.WriteLine(" greater number is " + num2);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
