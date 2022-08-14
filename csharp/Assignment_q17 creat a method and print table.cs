1c175521697a742730ec1b022e08044509ce81ed
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:14:57 +0530

    practice_q23

---
+++ csharp/Assignment_q17 creat a method and print table.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace create_a_method_and_print_table
+{
+    internal class Program
+    {
+        
+        static void table(int num) {
+            
+            for(int i = 1; i <= 10; i++)
+            {
+                int t = num * i;
+                Console.WriteLine("{0} * {1} = {2} ", num, i,t);
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int num = Convert.ToInt32(Console.ReadLine());
+             table(num);
+           
+            Console.ReadLine();
+        }
+    }
+}
