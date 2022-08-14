8cdc31d47a8ebba93e5102f6172ceebed22fa272
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:01:58 +0530

    practice_q7

---
+++ csharp/array rank and length properties.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_rank_and_length_properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[2, 3];
+            Console.WriteLine("Enter six number");
+            for(int i = 0; i < 2; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    array[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine("rank" + array.Rank);
+            Console.WriteLine("legth" + array.Length);
+            Console.ReadLine();
+
+        }
+    }
+}
