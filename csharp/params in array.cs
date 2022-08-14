00170018a56160b53f20f0727ccb9aa6d7f405a2
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:30:21 +0530

    practice_q47

---
+++ csharp/params in array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace params_in_array
+{
+    
+    internal class Program
+    {
+        static void display(params int[] num)
+        {
+         
+            Console.WriteLine(num[0]);
+            Console.WriteLine(num[1]);
+            Console.WriteLine(num[2]);
+            Console.WriteLine(num[3]);
+        
+
+        }
+        static void Main(string[] args)
+        {
+            display(3, 5, 7, 9);
+            Console.ReadLine();
+        }
+    }
+}
