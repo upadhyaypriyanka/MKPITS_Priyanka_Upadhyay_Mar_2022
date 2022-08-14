f2c9b5e0003b61c5f759133a765e943690613e3d
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:28:33 +0530

    practice_q44

---
+++ csharp/local and class variable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace local_and_class_variable
+{// local and class variable
+    internal class Program
+    {
+       static int gnum=20;
+        static void display()
+        {
+            int lnum = 10;
+            Console.WriteLine("local variable is = " + lnum);
+            Console.WriteLine("globle variable is = " + gnum);
+        }
+        static void Main(string[] args)
+        {
+            display();
+            //error since lnum is local variable
+            //Console.WriteLine("local variable is = " + lnum);
+
+            Console.WriteLine("globle variable is = " + gnum);
+            Console.ReadLine();
+        }
+    }
+}
