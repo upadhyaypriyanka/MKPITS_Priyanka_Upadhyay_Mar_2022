56e4dda8e937562868a6678ab6e018115a5692b3
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:20:49 +0530

    practice_q32

---
+++ csharp/double dimention array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace double_dimension_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] num = { { 5, 8, 8 }, { 8, 9, 4 } };
+            for(int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(num[i,j] +"\t");
+                }
+                Console.WriteLine();
+                
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
