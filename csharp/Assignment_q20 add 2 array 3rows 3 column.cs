2415743bae553a0fd88a5438eef4200ccf0ad631
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:16:27 +0530

    practice_q26

---
+++ csharp/Assignment_q20 add 2 array 3rows 3 column.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition_of_2_array_3rows_3colums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] num = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine("Enter numbers");
+                    num[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(num[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            int[,] num1 = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine("Enter numbers");
+                    num1[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(num1[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Addition of two array ");
+
+            int[,] num3 = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    num3[i, j] = num[i, j] + num1[i, j];
+                    //Console.WriteLine("Enter numbers");
+                    //num[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(num3[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
