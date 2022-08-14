477ee95b562f497b0e4272952f539389114cbe26
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:01:34 +0530

    practice_q6

---
+++ csharp/array initilization.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_inititialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 5, 7, 8, 9, 3, };
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(" num [{0}] = {1} ", i, num[i]);
+                Console.ReadLine();
+            }
+        }
+    }
+}
