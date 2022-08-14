3c47033e56225b9cc68e09d8c9cb8df45be48526
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:31:01 +0530

    practice_q48

---
+++ csharp/pass by referance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pass_by_reference
+{
+    internal class Program
+    {//pass by referance
+        static void display(ref int num)
+        {
+            num++;
+            Console.WriteLine("Value inside method " + num);
+        }
+        static void Main(string[] args)
+        {
+            int n = 6;
+            display(ref n);
+            Console.WriteLine("value inside main " + n);
+            Console.ReadLine();
+        }
+    }
+}
