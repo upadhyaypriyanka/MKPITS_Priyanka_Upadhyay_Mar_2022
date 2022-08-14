44a389c0c6403a375f8280d649dc5f2a10c14db2
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:24:35 +0530

    practice_q37

---
+++ csharp/example of out.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pass_by_reference
+{
+    internal class Program
+    {//pass by referance
+        static void display(out int num)
+        {
+            num = 8;
+            Console.WriteLine("Value inside method " + num);
+        }
+        static void Main(string[] args)
+        {
+            int n=9;
+            display(out n);
+            Console.WriteLine("value inside main " + n);
+            Console.ReadLine();
+        }
+    }
+}
