d61c007d36f940409fcceb0dded1ec84b69701b7
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:07:56 +0530

    practice_q13

---
+++ csharp/Assignment_q7 if else even number.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace even_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            // wap to accept a no. and print whether it is even no. or odd no.
+        {
+            int num;
+            Console.WriteLine("Enter number ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Even number = " + num);
+            }
+            else
+            {
+                Console.WriteLine("Odd number = " + num);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
