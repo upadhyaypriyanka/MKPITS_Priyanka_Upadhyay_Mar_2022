8d036f3e3475bdcba4db5fcc6aad5e3256a417c4
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:34:54 +0530

    practice_q54

---
+++ csharp/square.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace square
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter Number");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            int square = num * num;
+            Console.WriteLine("Square of the number is: " + square);
+            Console.ReadLine();
+        }
+    }
+}
