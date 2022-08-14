a959891fa66799e9d55fdd9934cb90024372ad81
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:29:16 +0530

    practice_q45

---
+++ csharp/operater if else.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operator_if_else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // wap to accept 2 no and operator like +,-,*,/ and display the result
+            int num1, num2, res=0;
+            char op;
+            Console.WriteLine("Enter number 1");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number 2");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter any operator like +,-,*,/");
+            op = Convert.ToChar(Console.ReadLine());
+           
+            if (op == '+')
+            {
+                res = num1 + num2;
+            }
+            else if (op == '-')
+            {
+                res = num1 - num2;
+            }
+            else if (op == '*')
+            {
+                res = num1 * num2;
+            }
+            else if (op == '/')
+            {
+                res = num1 / num2;
+            }
+            else
+            {
+                Console.WriteLine("Invalid operator");
+            }
+            Console.WriteLine("result= " + res);
+            Console.ReadLine();
+        }
+    }
+}
