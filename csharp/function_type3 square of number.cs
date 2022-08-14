1b4b839a6533ba471322367f186528dd2c9bc62d
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:26:59 +0530

    practice_q41

---
+++ csharp/function_type3 square of number.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace function_type3_square_of_numbers
+{
+    internal class Program
+    {  // method-3 with parameters and returning value
+    
+        static int square(int num) // called function
+        {
+            int squ = num * num;
+            return squ;
+         
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int num = Convert.ToInt32(Console.ReadLine());
+           int res= square(num); //calling function
+            Console.WriteLine(" square of a {0} is {1}", num, res);
+            Console.ReadLine();
+        }
+    }
+}
