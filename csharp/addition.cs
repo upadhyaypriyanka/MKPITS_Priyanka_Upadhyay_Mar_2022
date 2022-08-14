da76a77913a1657603430b245b1bf6db60bf0b55
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-15 00:00:08 +0530

    practice_q3

---
+++ csharp/addition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1,num2;
+            Console.WriteLine("Enter first number");
+            num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            num2=Convert.ToInt32(Console.ReadLine());
+            int add=num1+num2;
+            Console.WriteLine("Addition of two number is: "+add);
+            Console.ReadLine();
+        }
+    }
+}
