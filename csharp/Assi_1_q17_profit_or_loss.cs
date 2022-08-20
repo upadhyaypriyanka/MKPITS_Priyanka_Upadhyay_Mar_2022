6223ee09eeb99e2ec4d3743c3fd7353852cd351c
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-20 18:18:04 +0530

    Assignment_1_q17

---
+++ csharp/Assi_1_q17_profit_or_loss.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace profit_and_loss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pcost, pselling;
+            Console.Write("Enter cost price: ");
+            pcost = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter selling price: ");
+            pselling = Convert.ToInt32(Console.ReadLine());
+            if (pcost < pselling)
+            {
+                int profit =  pselling-pcost;
+                Console.WriteLine("You can booked your profit amount : {0}", profit);
+            }
+            else if (pcost > pselling)
+            {
+                int loss =  pcost-pselling;
+                Console.WriteLine("You got a loss of amount : {0}", loss);
+            }
+            else
+            {
+                Console.WriteLine("You are running in no profit no loss condition");
+            }
+            Console.ReadLine();
+        }
+    }
+}
