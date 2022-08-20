cdc5d58a6df80ff79219b2196b65dd0fbc7b7c9b
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-20 18:01:52 +0530

    Assignment_1

---
+++ csharp/Assi_1_q1_number_equal_or_not.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace number_equal_or_not
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("Enter first number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1 == num2)
+            {
+                Console.WriteLine("{0} and {1} are equal",num1,num2);
+            }
+            else
+            {
+                Console.WriteLine("{0} and {1} are not equal",num1,num2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
