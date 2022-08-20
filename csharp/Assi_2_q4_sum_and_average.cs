1c3d847f1fb72021e65b92a05d679f5e4a9311e4
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-20 18:23:07 +0530

    Assignment_2_q4

---
+++ csharp/Assi_2_q4_sum_and_average.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum_and_average
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine("Enter any 10 number\n");
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.Write("number {0}:",i);
+                
+                int n = Convert.ToInt32(Console.ReadLine());
+                sum = sum + n;
+                
+            }
+            float avg= sum / 10;
+            Console.WriteLine("Sum of 10 number is: {0}\n average is: {1}", sum, avg);
+            Console.ReadLine();
+        }
+    }
+}
