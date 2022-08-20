b111c07d18ac9e4016353e5ac0436a568604d3b6
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-20 18:22:08 +0530

    Assignment_2_q2

---
+++ csharp/Assi_2_q2_first_ten_natural_number_addition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_ten_natural_number_addition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int add = 0;
+            Console.WriteLine("First ten natural numbers\n");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(" " + i);
+                add = add + i;
+            }
+            Console.Write("\n\nThe addition of first ten number is: {0}", add);
+            Console.ReadLine();
+        }
+    }
+}
