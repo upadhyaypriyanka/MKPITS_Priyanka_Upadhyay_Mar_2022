40400803de4aaaf8412a53f68ca01805df6672a7
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-20 18:22:40 +0530

    Assignment_2_q3

---
+++ csharp/Assi_2_q3_display_n_terms_of_natural_number.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace n_terms_natural_number_addition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int add = 0,naturalNumber;
+            Console.WriteLine("Enter any natural number");
+            naturalNumber = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 1; i <= naturalNumber; i++)
+            {
+                Console.Write(" " + i);
+                add = add + i;
+            }
+            Console.Write("\n\nThe addition of first {0} natural number is: {1}", naturalNumber, add);
+            Console.ReadLine();
+        }
+    }
+}
