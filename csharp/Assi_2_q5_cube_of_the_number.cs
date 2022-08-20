64144d4f3b41326e823507012ff79a09738eac65
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-20 18:24:09 +0530

    Assignment_2_q5

---
+++ csharp/Assi_2_q5_cube_of_the_number.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cube_of_the_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of terms");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i <= num; i++)
+            {
+                int cube = i * i * i;
+                Console.WriteLine("Number is : {0} and cube of the {0} is :{1}", i, cube);
+            }
+            Console.ReadLine();
+        }
+    }
+}
