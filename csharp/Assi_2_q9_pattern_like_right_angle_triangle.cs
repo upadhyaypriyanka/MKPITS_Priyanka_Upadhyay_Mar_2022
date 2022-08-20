0fe3f7faabbbb92ef04f154c3178fb6effbe5feb
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-20 18:24:56 +0530

    Assignment_2_q9

---
+++ csharp/Assi_2_q9_pattern_like_right_angle_triangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pattern_like_right_angle_triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 4; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
