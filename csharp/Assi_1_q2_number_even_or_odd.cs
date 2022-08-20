642b6e381bcd12f6b8024eabfa2c0c5ba924e017
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-20 18:02:40 +0530

    Assignment_1_q2

---
+++ csharp/Assi_1_q2_number_even_or_odd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace number_even_or_odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            Console.WriteLine("Enter first number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            
+            if (num1%2==0)
+            {
+                Console.WriteLine("{0} is an even integer",num1);
+            }
+            else
+            {
+                Console.WriteLine("{0} is an odd integer",num1);
+            }
+            Console.ReadLine();
+        }
+    }
+}
