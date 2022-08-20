9386c514de0bbe397bb17af31128697564005e46
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-20 18:23:38 +0530

    Assignment_2_q6

---
+++ csharp/Assi_2_q6_mutiplication_table_of_given_number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplication_table_of_a_given_integer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int table = 1;
+            Console.Write("Enter number which table you want: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                table = num * i;
+                Console.Write("{0} * {1} = {2}\n", num, i, table);
+            }
+            Console.ReadLine();
+        }
+    }
+}
