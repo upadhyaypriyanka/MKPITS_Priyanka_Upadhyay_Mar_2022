9e199a61d44432664b50dc28af1f895f0cc9f48d
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-20 18:04:34 +0530

    Assignment_1_q7

---
+++ csharp/Assi_1_q7_categorize_person_according_their_height.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace categorize_the_person_according_to_thier_height
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int height;
+            Console.WriteLine("Enter height in centimeter");
+            height = Convert.ToInt32(Console.ReadLine());
+            if (height<150)
+            {
+                Console.WriteLine("The person is Dwarf.");
+            }
+            else if(height>=150 && height <= 165){
+                Console.WriteLine("The person is  average heighted.");
+            }
+            else if (height>=165 && height<=195)
+            {
+                Console.WriteLine("The person is taller.");
+            }
+            else
+            {
+                Console.WriteLine("Abnormal height.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
