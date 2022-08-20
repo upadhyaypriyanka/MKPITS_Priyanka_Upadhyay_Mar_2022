2db35b45dbd8ddd7a955eb21ec882b9c9ef7333e
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-20 18:19:13 +0530

    Assignment_1_q20

---
+++ csharp/Assi_1_q20_display_name_in_the_word.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace display_day_name_in_the_word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number(1-7):");
+            int daynum = Convert.ToInt32(Console.ReadLine());
+            if(daynum==1)
+            {
+                Console.WriteLine("Monday");
+            }
+            else if (daynum == 2)
+            {
+                Console.WriteLine("Tuesday");
+            }
+            else if (daynum == 3)
+            {
+                Console.WriteLine("Wednesday");
+            }
+            else if (daynum == 4)
+            {
+                Console.WriteLine("Thusday");
+            }
+            else if (daynum == 5)
+            {
+                Console.WriteLine("Friday");
+            }
+            else if (daynum == 6)
+            {
+                Console.WriteLine("Saturday");
+            }
+            else if (daynum == 7)
+            {
+                Console.WriteLine("Sunday");
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice");
+            }
+            Console.ReadLine();
+        }
+    }
+}
