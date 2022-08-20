b317da227f18f9e0d12f356a679c29a5b3f8783c
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-20 18:04:07 +0530

    Assignment_1_q5

---
+++ csharp/Assi_1_q5_eligible_for_vote_or_not.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eligible_for_casting_your_vote_or_not
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Enter  age");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            if ( age >=18)
+            {
+                Console.WriteLine("Congratulation! You are eligible for casting your vote");
+            }
+            else
+            {
+                Console.WriteLine("Sorry! You are not eligible for casting your vote. your age is less than 18");
+            }
+            Console.ReadLine();
+        }
+    }
+}
