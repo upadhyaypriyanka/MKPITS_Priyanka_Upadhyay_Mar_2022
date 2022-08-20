150bd02acd90fe32fb637244fa6fa56efc3ceb41
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-20 18:24:34 +0530

    Assignment_2_q8

---
+++ csharp/Assi_2_q8_odd_natural_number_and_their_sum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odd_natural_number_and_their_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { int n,sum=0;
+            Console.WriteLine("Enter number");
+            n = Convert.ToInt32(Console.ReadLine());
+            for(int i=1; i <= n; i++)
+            {
+                Console.WriteLine("{0}", 2 * i - 1);
+                int a = 2 * i - 1;
+                 sum = sum + a;
+            }
+            Console.WriteLine("Sum of odd number is: {0}", sum);
+            Console.ReadLine();
+        }
+    }
+}
