c269aaa6bb9effb26d72a04318ee99b4a58bd88d
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-20 18:21:38 +0530

    Assignment_2_q1

---
+++ csharp/Assi_2_q1_first_ten_natural_number.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_ten_natural_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First ten natural numbers");
+            for(int i=1;i<=10;i++){
+                Console.Write(" "+i);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
