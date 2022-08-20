3dc748e1e40cce1b8549195309373da005e0b54a
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-20 18:03:12 +0530

    Assignment_1_q3

---
+++ csharp/Assi_1_q3_number_positive_or_negative.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace number_positive_or_negative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            Console.WriteLine("Enter  number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1>0)
+            {
+                Console.WriteLine("{0} is a positive number",num1);
+            }
+            else
+            {
+                Console.WriteLine("{0} is a negative number",num1);
+            }
+            Console.ReadLine();
+        }
+    }
+}
