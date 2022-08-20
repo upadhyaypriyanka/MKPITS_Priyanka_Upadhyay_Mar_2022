6dbe346d2211b60205881a5b90c206f356fd1624
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-20 18:03:40 +0530

    Assignment_1_q4

---
+++ csharp/Assi_1_q4_year_leap_year_or_not.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace year_leap_year_or_not
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("Enter  year");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            if ((year%100==0 && year%400==0) || year%4==0)
+            {
+                Console.WriteLine("{0} is leap year", year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not leap year", year);
+            }
+            Console.ReadLine();
+        }
+    }
+}
