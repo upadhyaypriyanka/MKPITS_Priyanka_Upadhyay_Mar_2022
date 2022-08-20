417924a44d001b6962d2f1dfe59dad6829109b24
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-20 18:26:02 +0530

    Assignment_2_q12

---
+++ csharp/Assi_2_q12_triangle_with_number_increase_by1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triangle_with_number_increased_by_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { int k=1;
+            for(int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(k++);
+                }
+                Console.WriteLine("\n ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
