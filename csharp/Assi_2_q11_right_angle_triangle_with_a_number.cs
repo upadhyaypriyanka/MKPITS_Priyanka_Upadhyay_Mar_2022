f046e1b9f42a26c7228eda15d3d89a6cf12a0eee
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-20 18:25:38 +0530

    Assignment_2_q11

---
+++ csharp/Assi_2_q11_right_angle_triangle_with_a_number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace right_angle_triangle_with_a_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 4; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
