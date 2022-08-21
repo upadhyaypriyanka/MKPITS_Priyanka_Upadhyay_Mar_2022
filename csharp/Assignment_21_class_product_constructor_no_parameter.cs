c1dd30b472516b1c416640679ec1d735040449e5
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-21 15:05:30 +0530

    practice_q27

---
+++ csharp/Assignment_21_class_product_constructor_no_parameter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor_no_parameters_and_method_display
+{
+    class Product
+    {
+        
+        String productid, productname;
+        int price, quantity;
+       
+        public Product()
+        {
+            productid = "45647";
+            productname = "soap";
+            price = 100;
+            quantity = 2;
+
+            price = 200;
+        }
+        public void display()
+        {
+            Console.WriteLine("product id = " + productid);
+            Console.WriteLine("product name = " + productname);
+            Console.WriteLine(" quantity = " + quantity);
+            Console.WriteLine("price = " + price);
+
+        }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Product b = new Product();
+            b.display();
+            Console.ReadLine();
+        }
+    }
+}
