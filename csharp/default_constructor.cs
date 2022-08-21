87859e335dbd6c2faf2118093f8d5768cc62bb0c
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-21 15:07:26 +0530

    practice_q57

---
+++ csharp/default_constructor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace default_constructor_practice
+{
+    class Student
+    {
+        public int no;
+        public string name;
+
+        public void display()
+        {
+            Console.WriteLine("Student no = " + no);
+            Console.WriteLine("Student name = " + name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std = new Student();
+            std.no = 23;
+            std.name = "priyanka";
+            std.display();
+            Console.ReadLine();
+        }
+    }
+}
