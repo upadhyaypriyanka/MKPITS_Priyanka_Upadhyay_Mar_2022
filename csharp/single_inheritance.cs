72454f3b0a2ed8e733eb119d2167c2ec0be4150e
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-21 19:22:30 +0530

    practice_q63

---
+++ csharp/single_inheritance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace single_inheritance
+{
+    class Person
+    {
+        public int rno;
+    }
+    class Student : Person
+    {
+        public string name;
+    }
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.rno = 34;
+            s.name = "priyanka";
+            Console.WriteLine("Student roll no = " + s.rno);
+            Console.WriteLine("Student name = " + s.name);
+            Console.ReadLine();
+        }
+    }
+}
