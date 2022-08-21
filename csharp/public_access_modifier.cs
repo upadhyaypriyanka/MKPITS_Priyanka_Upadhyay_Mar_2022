1fd9707236adc3c9f0c296d25c31db8b4ff39984
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-21 15:31:36 +0530

    practice_q61

---
+++ csharp/public_access_modifier.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace public_access_modifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car mycar = new Car();
+            mycar.name = "ferrari";
+            mycar.color = "red";
+            mycar.model = 675899;
+            mycar.PrintCarInformation();
+            Console.ReadLine();
+        }
+
+    }
+    class Car
+    {
+        public string name, color;// by default private
+        public int model;
+
+        public void PrintCarInformation() // by default private
+        {
+            Console.WriteLine("Car name = " + name);
+            Console.WriteLine("Car color = " + color);
+            Console.WriteLine("Car model = " + model);
+        }
+       
+    }
+}
