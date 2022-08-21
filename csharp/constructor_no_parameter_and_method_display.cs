7c2c18e005bfc0487f1906315ccd0f9cb352651e
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-08-21 15:16:21 +0530

    practice_q59

---
+++ csharp/constructor_no_parameter_and_method_display.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor_no_parameters_and_method_display
+{
+    class Book
+    {
+        int bookid;
+        String title, author;
+        int price;
+        //creating a constructor with no parameters
+        public Book()
+        {
+            bookid = 1;
+            title = "oracle";
+            author = "james";
+
+            price = 200;
+        }
+        public void display()
+        {
+            Console.WriteLine("book id = " + bookid);
+            Console.WriteLine("book title = " + title);
+            Console.WriteLine("book author= " + author);
+            Console.WriteLine("price = " + price);
+
+        }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Book b = new Book();//this will call constructor automatically
+            b.display();
+            Console.ReadLine();
+        }
+    }
+}
