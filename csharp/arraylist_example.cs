45ade214c91da8e2bcd4ecd01e708247486618bd
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-09-02 12:49:14 +0530

    arraylist_example

---
+++ csharp/arraylist_example.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace _30_august_arraylist_display_value
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList ar = new ArrayList();
+            ar.Add(12);
+            ar.Add(14);
+            ar.Add(17);
+            ar.Add(13);
+            Console.WriteLine("array list colection with sorting");
+            Console.WriteLine("Student Roll No. ");
+            ar.Sort();
+            foreach(int obj in ar)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("Capacity = {0}" , ar.Capacity);
+            Console.WriteLine("Count = {0}" , ar.Count );
+            Console.ReadLine();
+        }
+    }
+}
