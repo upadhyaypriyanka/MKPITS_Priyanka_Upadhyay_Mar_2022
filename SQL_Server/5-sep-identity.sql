d8c56c900af351ccc282dc4eb1e60cc7a57e9e4a
upadhyaypriyanka <101799767+upadhyaypriyanka@users.noreply.github.com>
2022-09-18 20:35:13 +0530

    table_employee

---
+++ SQL_Server/5-sep-identity.sql
@@ -0,0 +1,13 @@
+create table employee (empid int primary key identity(1,1),ename varchar(20))
+insert into employee (ename) values('priti')
+select *from employee
+insert into employee (ename) values('priyanka')
+insert into employee (ename) values('gayatri')
+select *from employee
+delete from employee where empid=1
+insert into employee(ename) values('rajesh123')
+delete from employee
+select * from employee
+
+insert into employee(ename) values('rajesh123')
+select * from employee
